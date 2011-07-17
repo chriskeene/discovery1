d9d57675eef0dfe7bb704883c0e71bf06d4a1431
Chris Keene <chriskeene@gmail.com>
2011-07-17 18:31:10 +0100

    SPARQL connection now in its own class

---
+++ Sparqlendpoint.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Sparqlendpoint
+ *
+ * @author chriskeene
+ */
+class Sparqlendpoint {
+    
+    
+    protected $config;
+    protected $store;
+
+
+    
+    public function initalise () {
+        include_once("arc2/ARC2.php");
+        
+        $this->config['remote_store_endpoint'] = 'http://data.lib.cam.ac.uk/endpoint.php';
+        
+        
+        /* instantiation */
+        $this->store = ARC2::getRemoteStore($this->config);
+        
+        /* LOAD will call the Web reader, which will call the
+        format detector, which in turn triggers the inclusion of an
+        appropriate parser, etc. until the triples end up in the store. */
+        $this->store->query('LOAD <http://data.lib.cam.ac.uk/endpoint.php?>');
+
+        
+    }
+    
+    public function query ($q) {
+        $result = $this->store->query($q, 'rows');
+        return $result;
+    }
+
+
+
+
+
+
+}
+
+?>
--- one.php
+++ one.php
@@ -4,24 +4,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-include_once("arc2/ARC2.php");
 
-/* configuration */ 
-$config = array(
-  /* remote endpoint */
-  'remote_store_endpoint' => 'http://data.lib.cam.ac.uk/endpoint.php',
-);
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors',1);
 
-/* instantiation */
-$store = ARC2::getRemoteStore($config);
+include_once 'Sparqlendpoint.php';
+
+echo 'hello';
+
+$cambridgesparql = new Sparqlendpoint;
+$cambridgesparql->initalise();
 
 
-
-
-/* LOAD will call the Web reader, which will call the
-format detector, which in turn triggers the inclusion of an
-appropriate parser, etc. until the triples end up in the store. */
-$store->query('LOAD <http://data.lib.cam.ac.uk/endpoint.php?>');
 
 /* list names */
 $q = '
@@ -32,7 +27,7 @@ $q = '
 	 }
 ';
 $r = '';
-if ($rows = $store->query($q, 'rows')) {
+if ($rows = $cambridgesparql->query($q)) {
   foreach ($rows as $row) {
     $r .= '<li>' . $row['booktitle'] . '</li>';
   }
@@ -40,4 +35,5 @@ if ($rows = $store->query($q, 'rows')) {
 
 echo $r ? '<ul>' . $r . '</ul>' : 'no named persons found';
 
+echo $rows;
 ?>
